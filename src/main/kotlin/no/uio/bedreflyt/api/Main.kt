327671f7d1204f58441ce8fee4c7cbdbecfdf0f0
Riccardo Sieve <sievericcardo@gmail.com>
2024-10-27 20:01:00 +0100

    Set up main file

--- src/main/kotlin/no/uio/bedreflyt/api/Main.kt
+++ src/main/kotlin/no/uio/bedreflyt/api/Main.kt
@@ -1,5 +1,13 @@
-package no.uio.no.uio.bedreflyt.api
+package no.uio.bedreflyt.api
 
-fun main() {
-    println("Hello World!")
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
+
+@SpringBootApplication
+@ComponentScan(basePackages = ["no.uio.bedreflyt.api.config"])
+open class API
+
+fun main(args: Array<String>) {
+    SpringApplication.run(API::class.java, *args)
 }
