4577d40d466f7592cfc09b24d2c89365f01e6c53
Riccardo Sieve <sievericcardo@gmail.com>
2024-10-28 20:55:10 +0100

    Changed logger

--- src/main/kotlin/no/uio/bedreflyt/api/controller/HomeController.kt
+++ src/main/kotlin/no/uio/bedreflyt/api/controller/HomeController.kt
@@ -1,7 +1,6 @@
 package no.uio.bedreflyt.api.controller
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import java.util.logging.Logger
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 class HomeController {
 
-    private val log : Logger = LoggerFactory.getLogger(this.javaClass);
+    private val log : Logger = Logger.getLogger(HomeController::class.java.name)
 
     @GetMapping("/status")
     fun status(): String {
