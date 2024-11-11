1b76b57c2309001a53598bf3a2de61dcd0512e0e
Riccardo Sieve <sievericcardo@gmail.com>
2024-11-11 11:43:47 +0100

    Added support for simulation with SMOL

--- src/main/kotlin/no/uio/bedreflyt/api/controller/SimulationController.kt
+++ src/main/kotlin/no/uio/bedreflyt/api/controller/SimulationController.kt
@@ -1,24 +1,22 @@
 package no.uio.bedreflyt.api.controller
 
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import no.uio.bedreflyt.api.config.REPLConfig
-import no.uio.bedreflyt.api.model.simulation.Scenario
 import no.uio.bedreflyt.api.service.DatabaseService
 import no.uio.bedreflyt.api.service.live.PatientService
 import no.uio.bedreflyt.api.service.live.RoomDistributionService
 import no.uio.bedreflyt.api.service.live.RoomService
-import no.uio.bedreflyt.api.service.simulation.*
+import no.uio.microobject.runtime.REPL
+import org.apache.jena.query.QuerySolution
+import org.apache.jena.query.ResultSet
 import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.client.RestTemplate
 import java.io.BufferedReader
 import java.io.InputStreamReader
@@ -53,18 +51,6 @@ class SimulationController (
 ) {
 
     private val log : Logger = Logger.getLogger(HomeController::class.java.name)
-
-    @PostMapping("/smol")
-    fun smol() : ResponseEntity<String> {
-        val smolPath = System.getenv("SMOL_PATH") ?: "Bedreflyt.smol"
-
-        // get the repl from the config
-        val repl = replConfig.repl()
-        repl.command("read", smolPath)
-        repl.command("auto", "")
-
-        return ResponseEntity.ok("SMOL file read")
-    }
 
     private fun executeJar() : String {
         val command = listOf("java", "-jar", "bedreflyt.jar")
@@ -159,6 +145,49 @@ class SimulationController (
         }
     }
 
+    private fun simulate() : ResponseEntity<List<String>> {
+        try {
+            val data = executeJar()
+
+            // We need an Element Breaker to separate the information
+            val information = data.split("------").filter { it.isNotEmpty() } // EB - split data over ------
+
+            val groupedInformation = mutableListOf<List<String>>()
+            var currentGroup = mutableListOf<String>()
+
+            for (item in information) {
+                if (item.isNotEmpty()) {
+                    currentGroup.add(item)
+                } else {
+                    if (currentGroup.isNotEmpty()) {
+                        groupedInformation.add(currentGroup)
+                        currentGroup = mutableListOf()
+                    }
+                }
+            }
+
+            if (currentGroup.isNotEmpty()) {
+                groupedInformation.add(currentGroup)
+            }
+
+            val scenarios = mutableListOf<String>()
+
+            groupedInformation.forEach { group ->
+                group.forEach() { patient ->
+                    val solveData = invokeSolver(patient)
+                    scenarios.add(solveData)
+                    log.info(solveData)
+                }
+            }
+
+            return ResponseEntity.ok(scenarios)
+        } catch (e: Exception) {
+            "Error executing JAR: ${e.message}"
+            log.log(Level.SEVERE, "Error executing JAR", e)
+            return ResponseEntity.internalServerError().body(listOf("Error executing JAR"))
+        }
+    }
+
     @Operation(summary = "Simulate a scenario")
     @ApiResponses(value = [
         ApiResponse(responseCode = "200", description = "Scenario simulated"),
@@ -221,45 +250,151 @@ class SimulationController (
             }
         }
 
-        try {
-            val data = executeJar()
-
-            // We need an Element Breaker to separate the information
-            val information = data.split("------").filter { it.isNotEmpty() } // EB - split data over ------
-
-            val groupedInformation = mutableListOf<List<String>>()
-            var currentGroup = mutableListOf<String>()
-
-            for (item in information) {
-                if (item.isNotEmpty()) {
-                    currentGroup.add(item)
-                } else {
-                    if (currentGroup.isNotEmpty()) {
-                        groupedInformation.add(currentGroup)
-                        currentGroup = mutableListOf()
-                    }
-                }
-            }
-
-            if (currentGroup.isNotEmpty()) {
-                groupedInformation.add(currentGroup)
-            }
-
-            val scenarios = mutableListOf<String>()
-
-            groupedInformation.forEach { group ->
-                group.forEach() { patient ->
-                    val solveData = invokeSolver(patient)
-                    scenarios.add(solveData)
-                    log.info(solveData)
-                }
-            }
-
-            return ResponseEntity.ok(scenarios)
-        } catch (e: Exception) {
-            "Error executing JAR: ${e.message}"
-            log.log(Level.SEVERE, "Error executing JAR", e)
-            return ResponseEntity.internalServerError().body(listOf("Error executing JAR"))
-        }
+        return simulate()
     }
+
+    @Operation(summary = "Simulate a scenario using smol")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Scenario simulated"),
+        ApiResponse(responseCode = "400", description = "Invalid scenario"),
+        ApiResponse(responseCode = "401", description = "Unauthorized"),
+        ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden"),
+        ApiResponse(responseCode = "500", description = "Internal server error")
+    ])
+    @PostMapping("/smolScenario")
+    fun simulateSmolScenario (@SwaggerRequestBody(description = "Request to sign in a new user") @RequestBody scenario: List<ScenarioRequest>): ResponseEntity<List<String>> {
+        log.info("Simulating scenario")
+        val repl: REPL = replConfig.repl()
+
+        val roomDbUrl = "roomData.db"
+        databaseService.createRoomTables(roomDbUrl)
+
+        val rooms =
+            """
+               SELECT * WHERE {
+                ?obj a prog:Room ;
+                    prog:Room_roomId ?roomId ;
+                    prog:Room_roomDescription ?roomDescription .
+            }"""
+
+        val resultRooms: ResultSet = repl.interpreter!!.query(rooms)!!
+
+        while (resultRooms.hasNext()) {
+            val solution: QuerySolution = resultRooms.next()
+            val roomId = solution.get("?roomId").asLiteral().toString().toLong()
+            val roomDescription = solution.get("?roomDescription").asLiteral().toString()
+            databaseService.insertRoom(roomDbUrl, roomId, roomDescription)
+        }
+
+        val roomDistributions =
+            """
+               SELECT * WHERE {
+                ?obj a prog:RoomDistribution ;
+                    prog:RoomDistribution_roomNumber ?roomNumber ;
+                    prog:RoomDistribution_roomNumberModel ?roomNumberModel ;
+                    prog:RoomDistribution_room ?room ;
+                    prog:RoomDistribution_capacity ?capacity ;
+                    prog:RoomDistribution_bathroom ?bathroom .
+            }"""
+
+        val resultRoomDistribution: ResultSet = repl.interpreter!!.query(roomDistributions)!!
+
+        while (resultRoomDistribution.hasNext()) {
+            val solution: QuerySolution = resultRoomDistribution.next()
+            val roomNumber = solution.get("?roomNumber").asLiteral().toString().split("^^")[0].toLong()
+            val roomNumberModel = solution.get("?roomNumberModel").asLiteral().toString().split("^^")[0].toLong()
+            val room = solution.get("?room").asLiteral().toString().split("^^")[0].toLong()
+            val capacity = solution.get("?capacity").asLiteral().toString().split("^^")[0].toInt()
+            val bathroom = solution.get("?bathroom").asLiteral().toString().split("^^")[0].toBoolean()
+            databaseService.insertRoomDistribution(roomDbUrl, roomNumber, roomNumberModel, room, capacity, bathroom)
+        }
+
+        val scenarioDbUrl = "scData.db"
+        databaseService.createPatientTable(scenarioDbUrl)
+
+        val patients = patientService.findAll()
+        patients.forEach { patient ->
+            if (patient != null) {
+                databaseService.insertPatient(scenarioDbUrl, patient.patientId, patient.gender)
+                databaseService.insertPatientStatus(scenarioDbUrl, patient.patientId, patient.infectious, 0)
+            }
+        }
+
+        scenario.forEach { scenarioRequest ->
+            scenarioRequest.patientId?.let { patientId ->
+                scenarioRequest.treatmentName?.let { treatmentName ->
+                    try {
+                        patientService.findByPatientId(patientId)
+                    } catch (e: EmptyResultDataAccessException) {
+                        return ResponseEntity.badRequest().body(listOf("Patient not found"))
+                    }
+
+                    databaseService.insertScenario(
+                        scenarioDbUrl,
+                        scenarioRequest.batch,
+                        scenarioRequest.patientId,
+                        treatmentName
+                    )
+                }
+            }
+        }
+
+        val treatmentDbUrl = "trData.db"
+        databaseService.createTreatmentTables(treatmentDbUrl)
+
+        val tasks =
+            """
+               SELECT * WHERE {
+                ?obj a prog:Task ;
+                    prog:Task_taskName ?taskName ;
+                    prog:Task_durationAverage ?averageDuration ;
+                    prog:Task_bed ?bedCategory .
+            }"""
+
+        val resultTasks: ResultSet = repl.interpreter!!.query(tasks)!!
+        while (resultTasks.hasNext()) {
+            val solution: QuerySolution = resultTasks.next()
+            val taskName = solution.get("?taskName").asLiteral().toString()
+            val bedCategory = solution.get("?bedCategory").asLiteral().toString().split("^^")[0].toInt()
+            val averageDuration = solution.get("?averageDuration").asLiteral().toString().split("^^")[0].toDouble().toInt()
+            databaseService.insertTask(treatmentDbUrl, taskName, bedCategory, averageDuration)
+        }
+
+        val taskDependencies =
+            """
+               SELECT * WHERE {
+                ?obj a prog:TaskDependency ;
+                    prog:TaskDependency_taskName ?taskName ;
+                    prog:TaskDependency_taskDependency ?taskDependency .
+            }"""
+
+        val resultTaskDependencies: ResultSet = repl.interpreter!!.query(taskDependencies)!!
+        while (resultTaskDependencies.hasNext()) {
+            val solution: QuerySolution = resultTaskDependencies.next()
+            val taskName = solution.get("?taskName").asLiteral().toString()
+            val taskDependency = solution.get("?taskDependency").asLiteral().toString()
+            databaseService.insertTaskDependency(treatmentDbUrl, taskName, taskDependency)
+        }
+
+        val treatments = """
+            SELECT * WHERE {
+                ?obj a prog:JourneyStep ;
+                    prog:JourneyStep_diagnosis ?diagnosis ;
+                    prog:JourneyStep_journeyOrder ?journeyOrder ;
+                    prog:JourneyStep_task ?task .
+            }"""
+
+        val resultTreatments: ResultSet = repl.interpreter!!.query(treatments)!!
+        while (resultTreatments.hasNext()) {
+            val solution: QuerySolution = resultTreatments.next()
+            val diagnosis = solution.get("?diagnosis").asLiteral().toString()
+            val orderInJourney = solution.get("?journeyOrder").asLiteral().toString().split("^^")[0].toInt()
+            val task = solution.get("?task").asLiteral().toString()
+            databaseService.insertTreatment(treatmentDbUrl, diagnosis, orderInJourney, task)
+        }
+
+        return simulate()
+    }
+
+
 }
