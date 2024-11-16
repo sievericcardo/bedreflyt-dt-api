8a3d2a117e00a3f6e2cf550fce03aa13fda38e74
Riccardo Sieve <sievericcardo@gmail.com>
2024-11-16 13:44:31 +0100

    Added jpa

--- src/main/kotlin/no/uio/bedreflyt/api/Main.kt
+++ src/main/kotlin/no/uio/bedreflyt/api/Main.kt
@@ -3,9 +3,11 @@ package no.uio.bedreflyt.api
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
 @ComponentScan(basePackages = ["no.uio.bedreflyt.api.config"])
+@EnableJpaRepositories(basePackages = arrayOf("no.uio.bedreflyt.api.repository.live"))
 open class API
 
 fun main(args: Array<String>) {
