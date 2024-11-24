b6d60d05cb54d72845cad66c39c2de2f351cdf5f
Riccardo Sieve <sievericcardo@gmail.com>
2024-11-24 16:45:54 +0100

    Controller for CRUD operations on Diagnosis

---
+++ src/main/kotlin/no/uio/bedreflyt/api/controller/triplestore/DiagnosisController.kt
@@ -0,0 +1,308 @@
+package no.uio.bedreflyt.api.controller.triplestore
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import no.uio.bedreflyt.api.config.REPLConfig
+import no.uio.bedreflyt.api.model.triplestore.Diagnosis
+import org.apache.jena.query.QuerySolution
+import org.apache.jena.query.ResultSet
+import org.apache.jena.update.UpdateExecutionFactory
+import org.apache.jena.update.UpdateFactory
+import org.apache.jena.update.UpdateProcessor
+import org.apache.jena.update.UpdateRequest
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import io.swagger.v3.oas.annotations.parameters.RequestBody as SwaggerRequestBody
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.io.File
+import java.util.logging.Logger
+
+data class DiagnosisRequest (
+    val diagnosisName : String,
+)
+
+data class UpdateDiagnosisRequest (
+    val oldDiagnosisName : String,
+    val newDiagnosisName : String,
+)
+
+@RestController
+@RequestMapping("/api/fuseki/diagnosis")
+class DiagnosisController (
+    private val replConfig: REPLConfig
+) {
+
+    private val log : Logger = Logger.getLogger(DiagnosisController::class.java.name)
+
+    @Operation(summary = "Add a new diagnosis")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Diagnosis added"),
+        ApiResponse(responseCode = "400", description = "Invalid diagnosis"),
+        ApiResponse(responseCode = "401", description = "Unauthorized"),
+        ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden"),
+        ApiResponse(responseCode = "500", description = "Internal server error")
+    ])
+    @PostMapping("/create")
+    fun createDiagnosis (@SwaggerRequestBody(description = "Request to add a new patient") @RequestBody diagnosisRequest: DiagnosisRequest) : ResponseEntity<String> {
+        log.info("Creating diagnosis $diagnosisRequest")
+        val repl = replConfig.repl()
+
+        if (diagnosisRequest.diagnosisName.isEmpty()) {
+            return ResponseEntity.badRequest().body("Diagnosis name cannot be empty")
+        }
+
+        val host = System.getenv().getOrDefault("TRIPLESTORE_URL", "localhost")
+        val dataStore = System.getenv().getOrDefault("TRIPLESTORE_DATASET", "Bedreflyt")
+        val tripleStore = "http://$host:3030/$dataStore"
+        val prefix = System.getenv().getOrDefault("DOMAIN_PREFIX", "http://www.smolang.org/bedreflyt#")
+        val ttlPrefix = if (prefix.isNotEmpty()) prefix.dropLast(1) else prefix
+
+        val query = """
+            PREFIX : <$prefix>
+            
+            INSERT DATA {
+                :diagnosis_${diagnosisRequest.diagnosisName} a :Diagnosis ;
+                 :diagnosisName "${diagnosisRequest.diagnosisName}" .
+            }
+        """.trimIndent()
+
+        val updateRequest: UpdateRequest = UpdateFactory.create(query)
+        val fusekiEndpoint = "$tripleStore/update"
+        val updateProcessor: UpdateProcessor = UpdateExecutionFactory.createRemote(updateRequest, fusekiEndpoint)
+
+        try {
+            updateProcessor.execute()
+        } catch (e: Exception) {
+            return ResponseEntity.badRequest().body("Error: the update query could not be executed.")
+        }
+
+        repl.interpreter!!.tripleManager.regenerateTripleStoreModel()
+        repl.interpreter!!.evalCall(
+            repl.interpreter!!.getObjectNames("AssetModel")[0],
+            "AssetModel",
+            "reconfigure")
+
+        // Append to the file bedreflyt.ttl
+        val path = "bedreflyt.ttl"
+        val fileContent = File(path).readText(Charsets.UTF_8)
+        val newContent = """
+            $fileContent
+            
+            ###  http://$ttlPrefix/diagnosis_${diagnosisRequest.diagnosisName}
+            :diagnosis_${diagnosisRequest.diagnosisName} rdf:type owl:NamedIndividual ,
+                            :Diagnosis ;
+                :diagnosisName "${diagnosisRequest.diagnosisName}" .
+        """.trimIndent()
+
+        File(path).writeText(newContent)
+
+        return ResponseEntity.ok("Diagnosis added")
+    }
+
+    @Operation(summary = "Retrieve the diagnosis")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Diagnosis retrieved"),
+        ApiResponse(responseCode = "400", description = "Invalid diagnosis"),
+        ApiResponse(responseCode = "401", description = "Unauthorized"),
+        ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden"),
+        ApiResponse(responseCode = "500", description = "Internal server error")
+    ])
+    @GetMapping("/retrieve")
+    fun retrieveDiagnosis() : ResponseEntity<List<Any>> {
+        log.info("Retrieving diagnosis")
+        val repl = replConfig.repl()
+        val diagnosisList = mutableListOf<Any>()
+
+        val diagnosis = """
+            SELECT * WHERE {
+                ?obj a prog:Diagnosis ;
+                    prog:Diagnosis_diagnosisName ?name .
+            }"""
+
+        val resultDiagnosis: ResultSet = repl.interpreter!!.query(diagnosis)!!
+
+        if (!resultDiagnosis.hasNext()) {
+            return ResponseEntity.badRequest().body(listOf("No diagnosis found"))
+        }
+        while (resultDiagnosis.hasNext()) {
+            val solution: QuerySolution = resultDiagnosis.next()
+            val name = solution.get("?diagnosis").asLiteral().toString()
+            diagnosisList.add(Diagnosis(name))
+        }
+
+        return ResponseEntity.ok(diagnosisList)
+    }
+
+    @Operation(summary = "Update a diagnosis")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Diagnosis updated"),
+        ApiResponse(responseCode = "400", description = "Invalid diagnosis"),
+        ApiResponse(responseCode = "401", description = "Unauthorized"),
+        ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden"),
+        ApiResponse(responseCode = "500", description = "Internal server error")
+    ])
+    @PostMapping("/update")
+    fun updateDiagnosis(@SwaggerRequestBody(description = "Request to update a diagnosis") @RequestBody updateDiagnosisRequest: UpdateDiagnosisRequest) : ResponseEntity<String> {
+        log.info("Updating diagnosis $updateDiagnosisRequest")
+        val repl = replConfig.repl()
+
+        if (updateDiagnosisRequest.oldDiagnosisName.isEmpty() || updateDiagnosisRequest.newDiagnosisName.isEmpty()) {
+            return ResponseEntity.badRequest().body("Diagnosis name cannot be empty")
+        }
+
+        val host = System.getenv().getOrDefault("TRIPLESTORE_URL", "localhost")
+        val dataStore = System.getenv().getOrDefault("TRIPLESTORE_DATASET", "Bedreflyt")
+        val tripleStore = "http://$host:3030/$dataStore"
+        val prefix = System.getenv().getOrDefault("DOMAIN_PREFIX", "http://www.smolang.org/bedreflyt#")
+        val ttlPrefix = if (prefix.isNotEmpty()) prefix.dropLast(1) else prefix
+
+        val query = """
+            PREFIX : <$prefix>
+            
+            DELETE {
+                :diagnosis_${updateDiagnosisRequest.oldDiagnosisName} a :Diagnosis ;
+                 :diagnosisName "${updateDiagnosisRequest.oldDiagnosisName}" .
+            }
+            
+            INSERT {
+                :diagnosis_${updateDiagnosisRequest.newDiagnosisName} a :Diagnosis ;
+                 :diagnosisName "${updateDiagnosisRequest.newDiagnosisName}" .
+            }
+            
+            WHERE {
+                :diagnosis_${updateDiagnosisRequest.oldDiagnosisName} a :Diagnosis ;
+                 :diagnosisName "${updateDiagnosisRequest.oldDiagnosisName}" .
+            }
+        """.trimIndent()
+
+        val updateRequest: UpdateRequest = UpdateFactory.create(query)
+        val fusekiEndpoint = "$tripleStore/update"
+        val updateProcessor: UpdateProcessor = UpdateExecutionFactory.createRemote(updateRequest, fusekiEndpoint)
+
+        try {
+            updateProcessor.execute()
+        } catch (e: Exception) {
+            return ResponseEntity.badRequest().body("Error: the update query could not be executed.")
+        }
+
+        repl.interpreter!!.tripleManager.regenerateTripleStoreModel()
+        repl.interpreter!!.evalCall(
+            repl.interpreter!!.getObjectNames("AssetModel")[0],
+            "AssetModel",
+            "reconfigure"
+        )
+
+        // Update the object in the file bedreflyt.ttl
+        val path = "bedreflyt.ttl"
+        val fileContent = File(path).readText(Charsets.UTF_8)
+        val newContent = fileContent.replace(
+            """
+            ###  http://$ttlPrefix/diagnosis_${updateDiagnosisRequest.oldDiagnosisName}
+            :diagnosis_${updateDiagnosisRequest.oldDiagnosisName} rdf:type owl:NamedIndividual ,
+                            :Diagnosis ;
+                :diagnosisName "${updateDiagnosisRequest.oldDiagnosisName}" .
+            """.trimIndent(), """
+            ###  http://$ttlPrefix/diagnosis_${updateDiagnosisRequest.newDiagnosisName}
+            :diagnosis_${updateDiagnosisRequest.newDiagnosisName} rdf:type owl:NamedIndividual ,
+                            :Diagnosis ;
+                :diagnosisName "${updateDiagnosisRequest.newDiagnosisName}" .
+            """.trimIndent()
+        ).replace(
+            """
+            :diagnosis_${updateDiagnosisRequest.oldDiagnosisName} rdf:type owl:NamedIndividual ,
+                            :Diagnosis ;
+                :diagnosisName "${updateDiagnosisRequest.oldDiagnosisName}" .
+            """.trimIndent(), """
+            ###  http://$ttlPrefix/diagnosis_${updateDiagnosisRequest.newDiagnosisName}
+            :diagnosis_${updateDiagnosisRequest.newDiagnosisName} rdf:type owl:NamedIndividual ,
+                            :Diagnosis ;
+                :diagnosisName "${updateDiagnosisRequest.newDiagnosisName}" .
+            """.trimIndent()
+        )
+
+        File(path).writeText(newContent)
+
+        return ResponseEntity.ok("Diagnosis updated")
+    }
+
+    @Operation(summary = "Delete a diagnosis")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Diagnosis deleted"),
+        ApiResponse(responseCode = "400", description = "Invalid diagnosis"),
+        ApiResponse(responseCode = "401", description = "Unauthorized"),
+        ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden"),
+        ApiResponse(responseCode = "500", description = "Internal server error")
+    ])
+    @PostMapping("/delete")
+    fun deleteDiagnosis(@SwaggerRequestBody(description = "Request to delete a diagnosis") @RequestBody diagnosisRequest: DiagnosisRequest) : ResponseEntity<String> {
+        log.info("Deleting diagnosis $diagnosisRequest")
+        val repl = replConfig.repl()
+
+        if (diagnosisRequest.diagnosisName.isEmpty()) {
+            return ResponseEntity.badRequest().body("Diagnosis name cannot be empty")
+        }
+
+        val host = System.getenv().getOrDefault("TRIPLESTORE_URL", "localhost")
+        val dataStore = System.getenv().getOrDefault("TRIPLESTORE_DATASET", "Bedreflyt")
+        val tripleStore = "http://$host:3030/$dataStore"
+        val prefix = System.getenv().getOrDefault("DOMAIN_PREFIX", "http://www.smolang.org/bedreflyt#")
+        val ttlPrefix = if (prefix.isNotEmpty()) prefix.dropLast(1) else prefix
+
+        val query = """
+            PREFIX : <$prefix>
+            
+            DELETE {
+                :diagnosis_${diagnosisRequest.diagnosisName} a :Diagnosis ;
+                 :diagnosisName "${diagnosisRequest.diagnosisName}" .
+            }
+            
+            WHERE {
+                :diagnosis_${diagnosisRequest.diagnosisName} a :Diagnosis ;
+                 :diagnosisName "${diagnosisRequest.diagnosisName}" .
+            }
+        """.trimIndent()
+
+        val updateRequest: UpdateRequest = UpdateFactory.create(query)
+        val fusekiEndpoint = "$tripleStore/update"
+        val updateProcessor: UpdateProcessor = UpdateExecutionFactory.createRemote(updateRequest, fusekiEndpoint)
+
+        try {
+            updateProcessor.execute()
+        } catch (e: Exception) {
+            return ResponseEntity.badRequest().body("Error: the update query could not be executed.")
+        }
+
+        repl.interpreter!!.tripleManager.regenerateTripleStoreModel()
+        repl.interpreter!!.evalCall(
+            repl.interpreter!!.getObjectNames("AssetModel")[0],
+            "AssetModel",
+            "reconfigure"
+        )
+
+        // Remove the object from the file bedreflyt.ttl
+        val path = "bedreflyt.ttl"
+        val fileContent = File(path).readText(Charsets.UTF_8)
+        val newContent = fileContent.replace(
+            """
+            ###  http://$ttlPrefix/diagnosis_${diagnosisRequest.diagnosisName}
+            :diagnosis_${diagnosisRequest.diagnosisName} rdf:type owl:NamedIndividual ,
+                            :Diagnosis ;
+                :diagnosisName "${diagnosisRequest.diagnosisName}" .
+            """.trimIndent(), ""
+        ).replace(
+            """
+            :diagnosis_${diagnosisRequest.diagnosisName} rdf:type owl:NamedIndividual ,
+                            :Diagnosis ;
+                :diagnosisName "${diagnosisRequest.diagnosisName}" .
+            """.trimIndent(), ""
+        )
+
+        File(path).writeText(newContent)
+
+        return ResponseEntity.ok("Diagnosis deleted")
+    }
+}
