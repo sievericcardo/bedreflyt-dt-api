9105319de802b5769e133a0095e791a2dcfab988
Riccardo Sieve <sievericcardo@gmail.com>
2024-12-11 22:43:01 +0100

    Finished CRUD operations

--- src/main/kotlin/no/uio/bedreflyt/api/controller/triplestore/TaskDependencyController.kt
+++ src/main/kotlin/no/uio/bedreflyt/api/controller/triplestore/TaskDependencyController.kt
@@ -31,13 +31,16 @@ data class TreatmentRequest (
     val dependsOn: String
 )
 
-data class UpdateTreatmentRequest (
-    val oldDiagnosis: String,
-    val newDiagnosis: String,
-    val oldTask: String,
-    val newTask: TaskOrString,
+data class UpdateTaskDependencyRequest (
+    val diagnosis: String,
+    val taskName: String,
     val oldDependsOn: String,
     val newDependsOn: String
+)
+
+data class DeleteTaskDependencyRequest (
+    val diagnosis: String,
+    val taskName: String
 )
 
 @RestController
@@ -133,5 +136,81 @@ class TaskDependencyController (
         }
 
         return ResponseEntity.ok(taskDependenciesDict)
+    }
+
+    @Operation(summary = "Update a task dependency")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Task dependency updated"),
+        ApiResponse(responseCode = "400", description = "Invalid task dependency"),
+        ApiResponse(responseCode = "401", description = "Unauthorized"),
+        ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden"),
+        ApiResponse(responseCode = "500", description = "Internal server error")
+    ])
+    @PostMapping("/update")
+    fun updateTaskDependency(@SwaggerRequestBody(description = "Request to update a task dependency") @RequestBody updateTreatmentRequest: UpdateTaskDependencyRequest) : ResponseEntity<String> {
+        log.info("Updating task dependency $updateTreatmentRequest")
+
+        if (!taskDependencyService.updateTaskDependency(updateTreatmentRequest.diagnosis, updateTreatmentRequest.taskName, updateTreatmentRequest.oldDependsOn, updateTreatmentRequest.newDependsOn)) {
+            return ResponseEntity.badRequest().body("Task dependency does not exist")
+        }
+
+        replConfig.regenerateSingleModel().invoke("task dependencies")
+
+        // Update the object in the file bedreflyt.ttl
+        val path = "bedreflyt.ttl"
+        val oldContent = """
+            ###  $ttlPrefix/taskDependency_${updateTreatmentRequest.taskName}
+            :taskDependency_${updateTreatmentRequest.taskName} rdf:type owl:NamedIndividual ,
+                            :taskDependency ;
+                :diagnosisName "${updateTreatmentRequest.diagnosis}" ;
+                :taskDependent "${updateTreatmentRequest.taskName}" ;
+                :taskToWait "${updateTreatmentRequest.oldDependsOn}" .
+            """.trimIndent()
+        val newContent = """
+            ###  $ttlPrefix/taskDependency_${updateTreatmentRequest.taskName}
+            :taskDependency_${updateTreatmentRequest.taskName} rdf:type owl:NamedIndividual ,
+                            :taskDependency ;
+                :diagnosisName "${updateTreatmentRequest.diagnosis}" ;
+                :taskDependent "${updateTreatmentRequest.taskName}" ;
+                :taskToWait "${updateTreatmentRequest.newDependsOn}" .
+            """.trimIndent()
+
+        triplestoreService.replaceContentIgnoringSpaces(path, oldContent, newContent)
+
+        return ResponseEntity.ok("Task dependency updated")
+    }
+
+    @Operation(summary = "Delete a task dependency")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Task dependency deleted"),
+        ApiResponse(responseCode = "400", description = "Invalid task dependency"),
+        ApiResponse(responseCode = "401", description = "Unauthorized"),
+        ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden"),
+        ApiResponse(responseCode = "500", description = "Internal server error")
+    ])
+    @PostMapping("/delete")
+    fun deleteTaskDependency(@SwaggerRequestBody(description = "Request to delete a task dependency") @RequestBody taskRequest: DeleteTaskDependencyRequest) : ResponseEntity<String> {
+        log.info("Deleting task dependency $taskRequest")
+
+        if (!taskDependencyService.deleteTaskDependency(taskRequest.diagnosis, taskRequest.taskName)) {
+            return ResponseEntity.badRequest().body("Task dependency does not exist")
+        }
+
+        replConfig.regenerateSingleModel().invoke("task dependencies")
+
+        // Append to the file bedreflyt.ttl
+        val path = "bedreflyt.ttl"
+        val oldContent = """
+            ###  $ttlPrefix/taskDependency_${taskRequest.taskName}
+            :taskDependency_${taskRequest.taskName} rdf:type owl:NamedIndividual ,
+                            :taskDependency ;
+                :diagnosisName "${taskRequest.diagnosis}" ;
+                :taskDependent "${taskRequest.taskName}" ;
+                :taskToWait ?taskToWait .
+            """.trimIndent()
+
+        triplestoreService.replaceContentIgnoringSpaces(path, oldContent, "")
+
+        return ResponseEntity.ok("Task dependency deleted")
     }
 }
