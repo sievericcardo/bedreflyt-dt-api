c0798f0d2e3ec0dcf770c6e9860777b63f22f343
Riccardo Sieve <sievericcardo@gmail.com>
2025-01-14 21:18:51 +0100

    Fixed order

--- src/main/kotlin/no/uio/bedreflyt/api/model/triplestore/TaskDependency.kt
+++ src/main/kotlin/no/uio/bedreflyt/api/model/triplestore/TaskDependency.kt
@@ -1,8 +1,8 @@
 package no.uio.bedreflyt.api.model.triplestore
 
 class TaskDependency (
-    val diagnosis: String,
     val treatment: String,
+    val diagnosis: String,
     val task: String,
     val dependsOn: String
 )
