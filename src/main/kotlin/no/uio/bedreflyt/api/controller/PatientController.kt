0328a6403a682bc16795c46614f5d1fd38f86f88
Riccardo Sieve <sievericcardo@gmail.com>
2025-02-03 21:43:43 +0100

    Optimised import and moved endpoints

--- src/main/kotlin/no/uio/bedreflyt/api/controller/PatientController.kt
+++ src/main/kotlin/no/uio/bedreflyt/api/controller/PatientController.kt
@@ -3,26 +3,17 @@ package no.uio.bedreflyt.api.controller
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
-import jakarta.validation.constraints.Min
-import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.Pattern
 import no.uio.bedreflyt.api.model.live.Patient
 import no.uio.bedreflyt.api.service.live.PatientService
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PatchMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-import io.swagger.v3.oas.annotations.parameters.RequestBody as SwaggerRequestBody
-import java.time.LocalDateTime
-import java.util.logging.Logger
+import no.uio.bedreflyt.api.types.DeletePatientRequest
 import no.uio.bedreflyt.api.types.PatientRequest
 import no.uio.bedreflyt.api.types.UpdatePatientRequest
-import no.uio.bedreflyt.api.types.DeletePatientRequest
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RequestBody
+import java.time.LocalDateTime
+import java.util.logging.Logger
+import io.swagger.v3.oas.annotations.parameters.RequestBody as SwaggerRequestBody
 
 @RestController
 @RequestMapping("/api/patient")
@@ -57,6 +48,46 @@ class PatientController (
         patientService.savePatient(patient)
 
         return ResponseEntity.ok("Patient created")
+    }
+
+    @Operation(summary = "Get patient by patientId")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "Patient found"),
+        ApiResponse(responseCode = "400", description = "Invalid patient"),
+        ApiResponse(responseCode = "401", description = "Unauthorized"),
+        ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden"),
+        ApiResponse(responseCode = "500", description = "Internal server error")
+    ])
+    @GetMapping("/get/{patientId}")
+    fun getPatient(@SwaggerRequestBody(description = "Request to get a patient by patientId") @PathVariable patientId: String) : ResponseEntity<Patient> {
+        log.info("Getting patient")
+
+        if (patientId.isEmpty()) {
+            return ResponseEntity.badRequest().build()
+        }
+
+        val patient = patientService.findByPatientId(patientId) ?: return ResponseEntity.badRequest().build()
+
+        return ResponseEntity.ok(patient)
+    }
+
+    @Operation(summary = "Get all patients")
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "200", description = "All patients"),
+        ApiResponse(responseCode = "400", description = "Invalid patient"),
+        ApiResponse(responseCode = "401", description = "Unauthorized"),
+        ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden"),
+        ApiResponse(responseCode = "500", description = "Internal server error")
+    ])
+    @GetMapping("/retrieve")
+    fun getAllPatients() : ResponseEntity<HashMap<String, List<Patient>>> {
+        log.info("Getting all patients")
+        val patients = patientService.findAll()
+
+        val response = HashMap<String, List<Patient>>()
+        response["patients"] = patients.toList() as List<Patient>
+
+        return ResponseEntity.ok(response)
     }
 
     @Operation(summary = "Update a patient")
@@ -108,45 +139,5 @@ class PatientController (
         patientService.deletePatient(patient)
 
         return ResponseEntity.ok("Patient deleted")
-    }
-
-    @Operation(summary = "Get patient by patientId")
-    @ApiResponses(value = [
-        ApiResponse(responseCode = "200", description = "Patient found"),
-        ApiResponse(responseCode = "400", description = "Invalid patient"),
-        ApiResponse(responseCode = "401", description = "Unauthorized"),
-        ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden"),
-        ApiResponse(responseCode = "500", description = "Internal server error")
-    ])
-    @GetMapping("/get/{patientId}")
-    fun getPatient(@SwaggerRequestBody(description = "Request to get a patient by patientId") @PathVariable patientId: String) : ResponseEntity<Patient> {
-        log.info("Getting patient")
-
-        if (patientId.isEmpty()) {
-            return ResponseEntity.badRequest().build()
-        }
-
-        val patient = patientService.findByPatientId(patientId) ?: return ResponseEntity.badRequest().build()
-
-        return ResponseEntity.ok(patient)
-    }
-
-    @Operation(summary = "Get all patients")
-    @ApiResponses(value = [
-        ApiResponse(responseCode = "200", description = "All patients"),
-        ApiResponse(responseCode = "400", description = "Invalid patient"),
-        ApiResponse(responseCode = "401", description = "Unauthorized"),
-        ApiResponse(responseCode = "403", description = "Accessing the resource you were trying to reach is forbidden"),
-        ApiResponse(responseCode = "500", description = "Internal server error")
-    ])
-    @GetMapping("/retrieve")
-    fun getAllPatients() : ResponseEntity<HashMap<String, List<Patient>>> {
-        log.info("Getting all patients")
-        val patients = patientService.findAll()
-
-        val response = HashMap<String, List<Patient>>()
-        response["patients"] = patients.toList() as List<Patient>
-
-        return ResponseEntity.ok(response)
     }
 }
