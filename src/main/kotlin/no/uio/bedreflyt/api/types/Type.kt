aab87a673d6913e4981c3ea1b38db7fcddbb3f1f
Riccardo Sieve <sievericcardo@gmail.com>
2025-02-03 21:42:03 +0100

    Added patient requests to the type

--- src/main/kotlin/no/uio/bedreflyt/api/types/Type.kt
+++ src/main/kotlin/no/uio/bedreflyt/api/types/Type.kt
@@ -169,3 +169,26 @@ data class DeteleTreatmentRequest (
     val treatmentId: String,
     val diagnosis: String
 )
+
+data class PatientRequest (
+    val patientName: String,
+    val patientSurname: String,
+    val patientAddress: String,
+    val city: String,
+    val patientBirthdate: String,
+    val gender: String
+)
+
+data class UpdatePatientRequest (
+    val patientId: String,
+    val patientName: String?,
+    val patientSurname: String?,
+    val patientAddress: String?,
+    val city: String?,
+    val patientBirthdate: String?,
+    val gender: String?
+)
+
+data class DeletePatientRequest (
+    val patientId: String
+)
