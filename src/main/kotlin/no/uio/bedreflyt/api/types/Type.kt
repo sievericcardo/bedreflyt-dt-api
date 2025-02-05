d7ea7bb561ade2abe4a13bc90cb9eafab87e0208
Riccardo Sieve <sievericcardo@gmail.com>
2025-02-05 18:13:48 +0100

    Added types for requests on allocations

--- src/main/kotlin/no/uio/bedreflyt/api/types/Type.kt
+++ src/main/kotlin/no/uio/bedreflyt/api/types/Type.kt
@@ -193,3 +193,33 @@ data class UpdatePatientRequest (
 data class DeletePatientRequest (
     val patientId: String
 )
+
+data class PatientAllocationRequest (
+    val patientId: String,
+    val acute: Boolean,
+    val diagnosisCode: String,
+    val diagnosisName: String,
+    val acuteCategory: Int?,
+    val careCategory: Int?,
+    val monitoringCategory: Int?,
+    val careId: Int?,
+    val contagious: Boolean,
+    val roomNumber: Int?
+)
+
+data class UpdatePatientAllocationRequest (
+    val patientId: String,
+    val newAcute: Boolean?,
+    val newDiagnosisCode: String?,
+    val newDiagnosisName: String?,
+    val newAcuteCategory: Int?,
+    val newCareCategory: Int?,
+    val newMonitoringCategory: Int?,
+    val newCareId: Int?,
+    val newContagious: Boolean?,
+    val newRoomNumber: Int?
+)
+
+data class DeletePatientAllocationRequest (
+    val patientId: String
+)
