a2937d5b16619f1bd3414eaf574dad8536c2eb2a
Riccardo Sieve <sievericcardo@gmail.com>
2025-03-20 22:02:49 +0100

    Created types for ward

---
+++ src/main/kotlin/no/uio/bedreflyt/api/types/WardTypes.kt
@@ -0,0 +1,19 @@
+package no.uio.bedreflyt.api.types
+
+data class WardRequest (
+    val wardName: String,
+    val wardCode: String?,
+    val wardHospitalName: String,
+    val wardFloorNumber: Int
+)
+
+data class UpdateWardRequest (
+    val wardName: String,
+    val hospitalHospitalName: String,
+    val newWardFloorNumber: Int,
+)
+
+data class DeleteWardRequest (
+    val wardName: String,
+    val hospitalHospitalName: String
+)
