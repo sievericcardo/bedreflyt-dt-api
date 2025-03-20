1e126eed5303dcaadebb66e42870e56cc5628429
Riccardo Sieve <sievericcardo@gmail.com>
2025-03-20 22:33:29 +0100

    Update the service for treatment rooms

--- src/main/kotlin/no/uio/bedreflyt/api/service/triplestore/RoomService.kt
+++ src/main/kotlin/no/uio/bedreflyt/api/service/triplestore/RoomService.kt
@@ -3,6 +3,9 @@ package no.uio.bedreflyt.api.service.triplestore
 import no.uio.bedreflyt.api.config.REPLConfig
 import no.uio.bedreflyt.api.config.TriplestoreProperties
 import no.uio.bedreflyt.api.model.triplestore.Room
+import no.uio.bedreflyt.api.model.triplestore.TreatmentRoom
+import no.uio.bedreflyt.api.types.DeleteRoomRequest
+import no.uio.bedreflyt.api.types.RoomRequest
 import org.apache.jena.query.QuerySolution
 import org.apache.jena.query.ResultSet
 import org.apache.jena.update.UpdateExecutionFactory
@@ -13,26 +16,30 @@ import org.springframework.stereotype.Service
 
 @Service
 class RoomService (
-    private val replConfig: REPLConfig,
-    private val triplestoreProperties: TriplestoreProperties
+    replConfig: REPLConfig,
+    triplestoreProperties: TriplestoreProperties,
+    private val wardService: WardService,
+    private val hospitalService: HospitalService,
+    private val monitoringCategoryService: MonitoringCategoryService
 ) {
-
+    
     private val tripleStore = triplestoreProperties.tripleStore
     private val prefix = triplestoreProperties.prefix
     private val ttlPrefix = triplestoreProperties.ttlPrefix
     private val repl = replConfig.repl()
 
-    fun createRoom(roomNumber: Int, roomNumberModel: Int, room: Long, capacity: Int, bathroom: Int) : Boolean {
+    fun createRoom(request: RoomRequest) : Boolean {
         val query = """
-            PREFIX : <$prefix>
+            PREFIX bedreflyt: <$prefix>
             
             INSERT DATA {
-                :room$roomNumber a :Room ;
-                    :roomNumber $roomNumber ;
-                    :roomNumberModel $roomNumberModel ;
-                    :roomCategory $room ;
-                    :capacity $capacity ;
-                    :bathroom $bathroom .
+                bedreflyt:Room${request.roomNumber} rdf:type owl:NamedIndividual ,
+                           <https://w3id.org/rec/building/TreatmentRoom> ;
+                    bedreflyt:hasBathroom bedreflyt:Room${request.roomNumber}-1 ;
+                    bedreflyt:hasMonitoringStatus bedreflyt:${request.categoryDescription} ;
+                    bedreflyt:isAssignWard bedreflyt:${request.ward} ;
+                    bedreflyt:hasCapacityNrBeds ${request.capacity} ;
+                    bedreflyt:hasRoomNr ${request.roomNumber} .
             }
         """.trimIndent()
 
@@ -48,17 +55,23 @@ class RoomService (
         }
     }
 
-    fun getAllRooms() : List<Room>? {
-        val rooms: MutableList<Room> = mutableListOf()
+    fun getAllRooms() : List<TreatmentRoom>? {
+        val rooms: MutableList<TreatmentRoom> = mutableListOf()
 
         val query =  """
-            SELECT DISTINCT ?roomNumber ?roomNumberModel ?roomCategory ?capacity ?bathroom WHERE {
-                ?obj a prog:Room ;
-                    prog:Room_roomNumber ?roomNumber ;
-                    prog:Room_roomNumberModel ?roomNumberModel ;
-                    prog:Room_roomCategory ?roomCategory ;
-                    prog:Room_capacity ?capacity ;
-                    prog:Room_bathroom ?bathroom .
+            SELECT DISTINCT ?roomNumber ?capacity ?wardName ?hospitalName ?category WHERE {
+                ?obj a prog:TreatingRoom ;
+                    prog:TreatingRoom_roomNumber ?roomNumber ;
+                    prog:TreatingRoom_capacity ?capacity ;
+                    prog:TreatingRoom_treatingWard ?ward ;
+                    prog:TreatingRoom_hospital ?hospital ;
+                    prog:TreatingRoom_monitoringCategory ?monitoringCategory .
+                ?ward a prog:Ward ;
+                    prog:Ward_wardName ?wardName .
+                ?hospital a prog:Hospital ;
+                    prog:Hospital_hospitalCode ?hospitalName .
+                ?monitoringCategory a prog:MonitoringCategory ;
+                    prog:MonitoringCategory_category ?category .
             }"""
 
         val resultRooms: ResultSet = repl.interpreter!!.query(query)!!
@@ -69,27 +82,38 @@ class RoomService (
         while (resultRooms.hasNext()) {
             val solution: QuerySolution = resultRooms.next()
             val roomNumber = solution.get("?roomNumber").asLiteral().toString().split("^^")[0].toInt()
-            val roomNumberModel = solution.get("?roomNumberModel").asLiteral().toString().split("^^")[0].toInt()
-            val room = solution.get("?roomCategory").asLiteral().toString().split("^^")[0].toLong()
             val capacity = solution.get("?capacity").asLiteral().toString().split("^^")[0].toInt()
-            val bathroom = solution.get("?bathroom").asLiteral().toString().split("^^")[0].toInt()
-            val bathBool = bathroom == 1
-            rooms.add(Room(roomNumber, roomNumberModel, room, capacity, bathBool))
+            val wardName = solution.get("?wardName").asLiteral().toString()
+            val hospitalName = solution.get("?hospitalName").asLiteral().toString()
+            val category = solution.get("?category").asLiteral().toString().split("^^")[0].toInt()
+
+            val ward = wardService.getWardByNameAndHospital(wardName, hospitalName) ?: continue
+            val hospital = hospitalService.getHospitalByCode(hospitalName) ?: continue
+            val monitoringCategory = monitoringCategoryService.getCategoryByCategory(category) ?: continue
+
+            rooms.add(TreatmentRoom(roomNumber, capacity, ward, hospital, monitoringCategory))
         }
 
         return rooms
     }
 
-    fun getRoomByRoomNumber(roomNumber: Int): Room? {
+    fun getRoomByRoomNumber(roomNumber: Int): TreatmentRoom? {
         val query = """
-            SELECT DISTINCT ?roomNumber ?roomNumberModel ?roomCategory ?capacity ?bathroom WHERE {
-                ?obj a prog:Room ;
-                    prog:Room_roomNumber $roomNumber ;
-                    prog:Room_roomNumberModel ?roomNumberModel ;
-                    prog:Room_roomCategory ?roomCategory ;
-                    prog:Room_capacity ?capacity ;
-                    prog:Room_bathroom ?bathroom .
-            }"""
+            SELECT DISTINCT ?capacity ?wardName ?hospitalName ?category WHERE {
+                ?obj a prog:TreatingRoom ;
+                    prog:TreatingRoom_roomNumber $roomNumber ;
+                    prog:TreatingRoom_capacity ?capacity ;
+                    prog:TreatingRoom_treatingWard ?ward ;
+                    prog:TreatingRoom_hospital ?hospital ;
+                    prog:TreatingRoom_monitoringCategory ?monitoringCategory .
+                ?ward a prog:Ward ;
+                    prog:Ward_wardName ?wardName .
+                ?hospital a prog:Hospital ;
+                    prog:Hospital_hospitalCode ?hospitalName .
+                ?monitoringCategory a prog:MonitoringCategory ;
+                    prog:MonitoringCategory_description ?category .
+            }
+        """.trimIndent()
 
         val resultRooms: ResultSet = repl.interpreter!!.query(query)!!
         if (!resultRooms.hasNext()) {
@@ -97,42 +121,52 @@ class RoomService (
         }
 
         val solution: QuerySolution = resultRooms.next()
-        val roomNumberModel = solution.get("?roomNumberModel").asLiteral().toString().split("^^")[0].toInt()
-        val room = solution.get("?roomCategory").asLiteral().toString().split("^^")[0].toLong()
         val capacity = solution.get("?capacity").asLiteral().toString().split("^^")[0].toInt()
-        val bathroom = solution.get("?bathroom").asLiteral().toString().split("^^")[0].toInt()
-        val bathBool = bathroom == 1
-        return Room(roomNumber, roomNumberModel, room, capacity, bathBool)
+        val wardName = solution.get("?wardName").asLiteral().toString()
+        val hospitalName = solution.get("?hospitalName").asLiteral().toString()
+        val category = solution.get("?category").asLiteral().toString()
+
+        val ward = wardService.getWardByNameAndHospital(wardName, hospitalName) ?: return null
+        val hospital = hospitalService.getHospitalByCode(hospitalName) ?: return null
+        val monitoringCategory = monitoringCategoryService.getCategoryByDescription(category) ?: return null
+
+        return TreatmentRoom(roomNumber, capacity, ward, hospital, monitoringCategory)
     }
 
-    fun updateRoom(room: Room, newRoomNumberModel: Int, newRoom: Long, newCapacity: Int, newBathroom: Int) : Boolean {
-        val bathroom = if (room.bathroom) 1 else 0
+    fun updateRoom(room: TreatmentRoom, newCapacity: Int?, newWard: String?, newCategory: String?) : Boolean {
+        val capacity = newCapacity ?: room.capacity
+        val ward = newWard ?: room.treatmentWard.wardName
+        val category = newCategory?.split(" ")?.joinToString(" ") ?: room.monitoringCategory.description
+
         val query = """
-            PREFIX : <$prefix>
+            PREFIX bedreflyt: <$prefix>
             
             DELETE {
-                :room${room.roomNumber} a :Room ;
-                    :roomNumber ${room.roomNumber} ;
-                    :roomNumberModel ${room.roomNumberModel} ;
-                    :roomCategory ${room.roomCategory} ;
-                    :capacity ${room.capacity} ;
-                    :bathroom $bathroom .
+                bedreflyt:Room${room.roomNumber} rdf:type owl:NamedIndividual ,
+                           <https://w3id.org/rec/building/TreatmentRoom> ;
+                    bedreflyt:hasBathroom bedreflyt:Room${room.roomNumber}-1 ;
+                    bedreflyt:hasMonitoringStatus bedreflyt:${room.monitoringCategory.description} ;
+                    bedreflyt:isAssignWard bedreflyt:${room.treatmentWard.wardName} ;
+                    bedreflyt:hasCapacityNrBeds ${room.capacity} ;
+                    bedreflyt:hasRoomNr ${room.roomNumber} .
             }
             INSERT {
-                :room${room.roomNumber} a :Room ;
-                    :roomNumber ${room.roomNumber} ;
-                    :roomNumberModel $newRoomNumberModel ;
-                    :roomCategory $newRoom ;
-                    :capacity $newCapacity ;
-                    :bathroom $newBathroom .
+                bedreflyt:Room${room.roomNumber} rdf:type owl:NamedIndividual ,
+                           <https://w3id.org/rec/building/TreatmentRoom> ;
+                    bedreflyt:hasBathroom bedreflyt:Room${room.roomNumber}-1 ;
+                    bedreflyt:hasMonitoringStatus bedreflyt:$category ;
+                    bedreflyt:isAssignWard bedreflyt:$ward ;
+                    bedreflyt:hasCapacityNrBeds $capacity ;
+                    bedreflyt:hasRoomNr ${room.roomNumber} .
             }
             WHERE {
-                :room${room.roomNumber} a :Room ;
-                    :roomNumber ${room.roomNumber} ;
-                    :roomNumberModel ${room.roomNumberModel} ;
-                    :roomCategory ${room.roomCategory} ;
-                    :capacity ${room.capacity} ;
-                    :bathroom $bathroom .
+                bedreflyt:Room${room.roomNumber} rdf:type owl:NamedIndividual ,
+                           <https://w3id.org/rec/building/TreatmentRoom> ;
+                    bedreflyt:hasBathroom bedreflyt:Room${room.roomNumber}-1 ;
+                    bedreflyt:hasMonitoringStatus bedreflyt:${room.monitoringCategory.description} ;
+                    bedreflyt:isAssignWard bedreflyt:${room.treatmentWard.wardName} ;
+                    bedreflyt:hasCapacityNrBeds ${room.capacity} ;
+                    bedreflyt:hasRoomNr ${room.roomNumber} .
             }
         """.trimIndent()
 
@@ -148,28 +182,29 @@ class RoomService (
         }
     }
 
-    fun deleteRoom(roomNumber: Int) : Boolean {
-        val room = getRoomByRoomNumber(roomNumber) ?: return true
-        val bathroom = if (room.bathroom) 1 else 0
+    fun deleteRoom(request: DeleteRoomRequest) : Boolean {
+        val room = getRoomByRoomNumber(request.roomNumber) ?: return false
 
         val query = """
-            PREFIX : <$prefix>
+            PREFIX bedreflyt: <$prefix>
             
             DELETE {
-                :room$roomNumber a :Room ;
-                    :roomNumber $roomNumber ;
-                    :roomNumberModel ${room.roomNumberModel} ;
-                    :roomCategory ${room.roomCategory} ;
-                    :capacity ${room.capacity} ;
-                    :bathroom $bathroom .
+                bedreflyt:Room${room.roomNumber} rdf:type owl:NamedIndividual ,
+                           <https://w3id.org/rec/building/TreatmentRoom> ;
+                    bedreflyt:hasBathroom bedreflyt:Room${room.roomNumber}-1 ;
+                    bedreflyt:hasMonitoringStatus bedreflyt:${room.monitoringCategory.description} ;
+                    bedreflyt:isAssignWard bedreflyt:${room.treatmentWard.wardName} ;
+                    bedreflyt:hasCapacityNrBeds ${room.capacity} ;
+                    bedreflyt:hasRoomNr ${room.roomNumber} .
             }
             WHERE {
-                :room$roomNumber a :Room ;
-                    :roomNumber $roomNumber ;
-                    :roomNumberModel ${room.roomNumberModel} ;
-                    :roomCategory ${room.roomCategory} ;
-                    :capacity ${room.capacity} ;
-                    :bathroom ${bathroom} .
+                bedreflyt:Room${room.roomNumber} rdf:type owl:NamedIndividual ,
+                           <https://w3id.org/rec/building/TreatmentRoom> ;
+                    bedreflyt:hasBathroom bedreflyt:Room${room.roomNumber}-1 ;
+                    bedreflyt:hasMonitoringStatus bedreflyt:${room.monitoringCategory.description} ;
+                    bedreflyt:isAssignWard bedreflyt:${room.treatmentWard.wardName} ;
+                    bedreflyt:hasCapacityNrBeds ${room.capacity} ;
+                    bedreflyt:hasRoomNr ${room.roomNumber} .
             }
         """.trimIndent()
 
