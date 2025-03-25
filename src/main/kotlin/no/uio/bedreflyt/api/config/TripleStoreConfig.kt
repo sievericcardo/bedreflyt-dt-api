6698755b48cd605318eadf3abecac2eb140356fd
Riccardo Sieve <sievericcardo@gmail.com>
2025-03-25 14:51:00 +0100

    Changed default prefix

--- src/main/kotlin/no/uio/bedreflyt/api/config/TripleStoreConfig.kt
+++ src/main/kotlin/no/uio/bedreflyt/api/config/TripleStoreConfig.kt
@@ -13,7 +13,7 @@ open class TriplestoreConfig (
         val host = environmentConfig.getOrDefault("TRIPLESTORE_URL", "localhost")
         val dataStore = environmentConfig.getOrDefault("TRIPLESTORE_DATASET", "Bedreflyt")
         val tripleStore = "http://$host:3030/$dataStore"
-        val prefix = environmentConfig.getOrDefault("DOMAIN_PREFIX", "http://www.smolang.org/bedreflyt#")
+        val prefix = environmentConfig.getOrDefault("DOMAIN_PREFIX", "http://www.smolang.org/bedreflyt/")
         val ttlPrefix = if (prefix.isNotEmpty()) prefix.dropLast(1) else prefix
         return TriplestoreProperties(tripleStore, prefix, ttlPrefix)
     }
