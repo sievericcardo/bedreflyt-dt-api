ee47089e0746452f7eaf3fedbfaa0315bb0d405c
Riccardo Sieve <sievericcardo@gmail.com>
2025-04-18 11:43:35 +0200

    Added Serializable for caching

--- src/main/kotlin/no/uio/bedreflyt/api/model/triplestore/MonitoringCategory.kt
+++ src/main/kotlin/no/uio/bedreflyt/api/model/triplestore/MonitoringCategory.kt
@@ -1,6 +1,13 @@
 package no.uio.bedreflyt.api.model.triplestore
 
-class MonitoringCategory (
+import java.io.Serializable
+
+data class MonitoringCategory (
     val category: Int,
     val description: String
-)
+) : Serializable
+{
+    override fun toString(): String {
+        return "MonitoringCategory(category=$category, description='$description')"
+    }
+}
