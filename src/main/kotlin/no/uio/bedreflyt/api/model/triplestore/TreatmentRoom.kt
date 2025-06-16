4f25e70d2556a864bbbed48609e9d17c37e95b67
Riccardo Sieve <sievericcardo@gmail.com>
2025-06-16 11:13:23 +0200

    Added penalty to treatment room with default value set to 0

--- src/main/kotlin/no/uio/bedreflyt/api/model/triplestore/TreatmentRoom.kt
+++ src/main/kotlin/no/uio/bedreflyt/api/model/triplestore/TreatmentRoom.kt
@@ -3,10 +3,11 @@ package no.uio.bedreflyt.api.model.triplestore
 class TreatmentRoom (
     roomNumber: Int,
     capacity: Int,
+    penalty: Double = 0.0,
     val treatmentWard: Ward,
     val hospital: Hospital,
     val monitoringCategory: MonitoringCategory
-) : Room (roomNumber, capacity) {
+) : Room (roomNumber, capacity, penalty) {
     val ward: Ward
         get() = treatmentWard // Custom getter for SpEL compatibility
 
